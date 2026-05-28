3f8d88042df2bc103aeacb3a08170c0e8e26cc9c
miurama <miurama@C010423593.zipangu.cac.co.jp>
2026-05-28 17:29:55 +0900

    練習問題05-03、作業2まで完了(05/28)

--- spring_practice/src/main/java/jp/co/sss/practice/p05/q01/controller/Practice0501Controller.java
+++ spring_practice/src/main/java/jp/co/sss/practice/p05/q01/controller/Practice0501Controller.java
@@ -1,5 +1,22 @@
 package jp.co.sss.practice.p05.q01.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sss.practice.p05.repository.FruitsSeasonRepository;
+
 // Practice6-01
+@Controller
 public class Practice0501Controller {
+
+	@Autowired
+	FruitsSeasonRepository repository;
+
+	@RequestMapping("/fruits/list/all")
+	public String showFruitsSeasonList(Model model) {
+		model.addAttribute("fruits_season", repository.findAll());
+		return "practice05/01/fruits_list";
+	}
 }
